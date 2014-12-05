e7de6a89f1074a288fec49744655317cc68f4cb5
dpavic <drazen.pavic@gmail.com>
2014-12-05 16:36:08 +0100

    configuration of Admin classes

--- src/Dpavic/JobsBundle/Admin/CategoryAdmin.php
+++ src/Dpavic/JobsBundle/Admin/CategoryAdmin.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Dpavic\JobsBundle\Admin;
 
 use Sonata\AdminBundle\Admin\Admin;
@@ -9,6 +10,29 @@ use Sonata\AdminBundle\Form\FormMapper;
 
 class CategoryAdmin extends Admin
 {
-    
-}
 
+    //setup the default sort column and order
+    protected $datagridValues = array(
+        '_sortOrder' => 'ASC',
+        '_sortBy' => 'name'
+    );
+
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper->add('name')
+                ->add('slug')
+        ;
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $filter)
+    {
+        $filter->add('name');
+    }
+
+    protected function configureListFields(ListMapper $list)
+    {
+        $list->addIdentifier('name')
+                ->add('slug');
+    }
+
+}
--- src/Dpavic/JobsBundle/Admin/JobAdmin.php
+++ src/Dpavic/JobsBundle/Admin/JobAdmin.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Dpavic\JobsBundle\Admin;
 
 use Sonata\AdminBundle\Admin\Admin;
@@ -11,6 +12,83 @@ use Dpavic\JobsBundle\Entity\Job;
 
 class JobAdmin extends Admin
 {
-    
-}
 
+    //setup the default sort column and order
+    protected $datagridValues = array(
+        '_sortOrder' => 'DESC',
+        '_sortBy' => 'createdAt'
+    );
+
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+                ->add('category')
+                ->add('type', 'choice', array('choices' => Job::getTypes(), 'expanded' => true))
+                ->add('company')
+                ->add('file', 'file', array('label' => 'Company Logo', 'required' => false))
+                ->add('url')
+                ->add('position')
+                ->add('location')
+                ->add('description')
+                ->add('howToApply')
+                ->add('isPublic')
+                ->add('email')
+                ->add('isActivated')
+        ;
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $filter)
+    {
+        $filter
+                ->add('category')
+                ->add('company')
+                ->add('position')
+                ->add('description')
+                ->add('isActivated')
+                ->add('isPublic')
+                ->add('email')
+                ->add('expiresAt')
+        ;
+    }
+
+    protected function configureListFields(ListMapper $list)
+    {
+        $list->addIdentifier('company')
+                ->add('position')
+                ->add('location')
+                ->add('url')
+                ->add('isActivated')
+                ->add('email')
+                ->add('category')
+                ->add('expiresAt')
+                ->add('_action', 'actions', array(
+                    'actions' => array(
+                        'view' => array(),
+                        'edit' => array(),
+                        'delete' => array(),
+                    )
+        ));
+    }
+
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+                ->add('category')
+                ->add('type')
+                ->add('company')
+                ->add('webPath', 'string', array('template' =>
+                    'DpavicJobsBundle:JobAdmin:list_image.html.twig'))
+                ->add('url')
+                ->add('position')
+                ->add('location')
+                ->add('description')
+                ->add('howToApply')
+                ->add('isPublic')
+                ->add('isActivated')
+                ->add('token')
+                ->add('email')
+                ->add('expiresAt')
+        ;
+    }
+
+}
